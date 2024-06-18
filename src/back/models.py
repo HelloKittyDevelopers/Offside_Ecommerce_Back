3aa5a9575fc5bce411d57f80f78d8e266cea9d2d
juanex2001 <juanseg201@gmail.com>
2024-06-17 20:41:56 -0500

    feat: Updated Model

--- src/back/models.py
+++ src/back/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    id_category = models.IntegerField(unique=True)
-    category = models.CharField(max_length=80)
-    category_product_id = models.AutoField(primary_key=True)
+    id_category = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=80, null=False)
+    category_product_id = models.IntegerField(null=False)
 
     class Meta:
         constraints = [
@@ -14,8 +14,8 @@ class Category(models.Model):
 
 class Image(models.Model):
     id_image = models.AutoField(primary_key=True)
-    image = models.BinaryField()
-    product_image = models.ForeignKey('Product', on_delete=models.CASCADE)
+    image = models.BinaryField(null=False)
+    product_image = models.ForeignKey('Product', on_delete=models.CASCADE, null=False)
 
 class OrderState(models.Model):
     id_order_state = models.AutoField(primary_key=True)
@@ -23,79 +23,78 @@ class OrderState(models.Model):
 
 class UserInfo(models.Model):
     id_user = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=80)
-    encrypted_password = models.CharField(max_length=80)
-    user_name = models.CharField(max_length=80)
-    email = models.CharField(max_length=80)
-    phone_number = models.CharField(max_length=80, null=True)
+    username = models.CharField(max_length=80, null=False)
+    encrypted_password = models.CharField(max_length=80, null=False)
+    user_name = models.CharField(max_length=80, null=False)
+    email = models.CharField(max_length=80, null=False)
+    phone_number = models.CharField(max_length=80)
 
 class Product(models.Model):
     id_product = models.AutoField(primary_key=True)
-    product_name = models.CharField(max_length=4000)
-    price = models.IntegerField()
-    description = models.TextField(null=True, blank=True)
-    stock = models.IntegerField()
-    type = models.ForeignKey('Type', on_delete=models.SET_NULL, null=True)
+    product_name = models.CharField(max_length=4000, null=False)
+    price = models.IntegerField(null=False)
+    description = models.TextField(blank=True)
+    stock = models.IntegerField(null=False)
+    type_id_category = models.ForeignKey('Type', on_delete=models.SET_NULL, null=True)
 
 class ProductCategory(models.Model):
-    category_product = models.ForeignKey(Category, on_delete=models.CASCADE)
-    product_category = models.ForeignKey(Product, on_delete=models.CASCADE)
+    category_product_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_category_id = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['category_product', 'product_category'], name='productcategory_16_pk')
+            models.UniqueConstraint(fields=['category_product_id', 'product_category_id'], name='productcategory_16_pk')
         ]
 
 class ProductOrder(models.Model):
-    product_order = models.ForeignKey(Product, on_delete=models.CASCADE)
-    order_product = models.ForeignKey('OrderUser', on_delete=models.CASCADE)
+    product_order_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order_product_id = models.ForeignKey(OrderUser, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['product_order', 'order_product'], name='productorder_pk')
+            models.UniqueConstraint(fields=['product_order_id', 'order_product_id'], name='productorder_pk')
         ]
 
 class ProductSize(models.Model):
-    product_size = models.ForeignKey(Product, on_delete=models.CASCADE)
-    size_product = models.ForeignKey('SizeProduct', on_delete=models.CASCADE)
+    product_size_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size_product_id = models.ForeignKey(SizeProduct, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['product_size', 'size_product'], name='productsize_pk')
+            models.UniqueConstraint(fields=['product_size_id', 'size_product_id'], name='productsize_pk')
         ]
 
 class Rol(models.Model):
     id_rol = models.AutoField(primary_key=True)
-    rol = models.CharField(max_length=80)
+    rol = models.CharField(max_length=80, null=False)
 
 class SizeProduct(models.Model):
     id_size = models.AutoField(primary_key=True)
-    size_prod = models.CharField(max_length=80)
+    size_prod = models.CharField(max_length=80. null=False)
 
 class Stock(models.Model):
     id_stock = models.AutoField(primary_key=True)
-    quantity = models.CharField(max_length=80)
-    product_size = models.ForeignKey(ProductSize, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=80, null=False)
+    product_size_id = models.ForeignKey(ProductSize, on_delete=models.CASCADE)
 
 class Type(models.Model):
     id_type = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=80)
+    type = models.CharField(max_length=80, null=False)
 
 class UserRol(models.Model):
-    user_rol = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-    rol_user = models.ForeignKey(Rol, on_delete=models.CASCADE)
+    user_rol_id = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+    rol_user_id = models.ForeignKey(Rol, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['user_rol', 'rol_user'], name='usuario_rol_pk')
+            models.UniqueConstraint(fields=['user_rol_id', 'rol_user_id'], name='usuario_rol_pk')
         ]
 
 class OrderUser(models.Model):
     id_order = models.AutoField(primary_key=True)
-    date_order = models.DateField()
-    total_payment = models.FloatField()
-    address = models.CharField(max_length=80)
-    user_user = models.ForeignKey(UserInfo, on_delete=models.SET_NULL, null=True)
-    order_state = models.ForeignKey(OrderState, on_delete=models.SET_NULL, null=True)
-    taxes = models.FloatField()
-
+    date_order = models.DateField(null=False)
+    total_payment = models.FloatField(null=False)
+    address = models.CharField(max_length=80, null=False)
+    user_user_id = models.ForeignKey(UserInfo, on_delete=models.SET_NULL, null=False)
+    order_state_id = models.ForeignKey(OrderState, on_delete=models.SET_NULL, null=False)
+    taxes = models.FloatField(null=False)
