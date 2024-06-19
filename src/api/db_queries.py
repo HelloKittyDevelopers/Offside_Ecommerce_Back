094d809a55a37a741ef1b2c2c862043c3cdbea9f
juanex2001 <juanseg201@gmail.com>
2024-06-19 15:47:22 -0500

    feat: added simple db queries

--- src/api/db_queries.py
+++ src/api/db_queries.py
@@ -6,6 +6,7 @@ from .models import (
     Stock, Type, UserRol, OrderUser
 )
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import connection
 
 # Product Functions
 def get_all_products():
@@ -13,13 +14,48 @@ def get_all_products():
 
 def get_product_by_id(product_id):
     try:
-        return Product.objects.get(id_product=product_id)
+        # Get product with respective id
+        cursor.execute("""
+            SELECT * FROM api_product WHERE id_product = %s
+        """, [product_id])
+        product = cursor.fetchone()
+
+        return product
     except Product.DoesNotExist:
         return None
 
 def get_products_by_category(category_name):
-    Category.objects.filter(catergory = category_name)
-    return Product.objects.filter(
-        id_product__in=ProductCategory.objects.filter(category_product_id=category_id).values('product_category_id')
-    )
+    with connection.cursor() as cursor:
+        # Get the category id for the given category name
+        cursor.execute("""
+            SELECT id_category FROM api_category WHERE category = %s
+        """, [category_name])
+        category_id = cursor.fetchone()
+        
+        if category_id is None:
+            return []
+        
+        category_id = category_id[0]
+
+        # Get product ids associated with the category id
+        cursor.execute("""
+            SELECT product_category_id FROM api_productcategory WHERE category_product_id = %s
+        """, [category_id])
+        product_ids = cursor.fetchall()
+
+        # Convert list of tuples to list of ids
+        product_ids = [pid[0] for pid in product_ids]
+
+        if not product_ids:
+            return []
+
+        # Get products with the obtained product ids
+        product_ids_tuple = tuple(product_ids)
+        cursor.execute("""
+            SELECT * FROM api_product WHERE id_product IN %s
+        """, [product_ids_tuple])
+        products = cursor.fetchall()
+    
+    return products
+
 
