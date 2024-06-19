b6c538eb00a5ee58b84effbbcb2b998bb9c109ef
juanex2001 <juanseg201@gmail.com>
2024-06-19 16:14:29 -0500

    fix: updated queries with raw/params

--- src/api/db_queries.py
+++ src/api/db_queries.py
@@ -14,48 +14,34 @@ def get_all_products():
 
 def get_product_by_id(product_id):
     try:
-        # Get product with respective id
-        cursor.execute("""
+        query = """
             SELECT * FROM api_product WHERE id_product = %s
-        """, [product_id])
-        product = cursor.fetchone()
-
-        return product
+        """
+        product = Product.objects.raw(query, [product_id])
+        return product[0] if product else None
     except Product.DoesNotExist:
         return None
 
+
 def get_products_by_category(category_name):
-    with connection.cursor() as cursor:
-        # Get the category id for the given category name
-        cursor.execute("""
-            SELECT id_category FROM api_category WHERE category = %s
-        """, [category_name])
-        category_id = cursor.fetchone()
-        
-        if category_id is None:
-            return []
-        
-        category_id = category_id[0]
-
-        # Get product ids associated with the category id
-        cursor.execute("""
-            SELECT product_category_id FROM api_productcategory WHERE category_product_id = %s
-        """, [category_id])
-        product_ids = cursor.fetchall()
-
-        # Convert list of tuples to list of ids
-        product_ids = [pid[0] for pid in product_ids]
-
-        if not product_ids:
-            return []
-
-        # Get products with the obtained product ids
-        product_ids_tuple = tuple(product_ids)
-        cursor.execute("""
-            SELECT * FROM api_product WHERE id_product IN %s
-        """, [product_ids_tuple])
-        products = cursor.fetchall()
+    # Get the category id for the given category name
+    category = Category.objects.filter(category=category_name).first()
+    if not category:
+        return Product.objects.none()
     
+    category_id = category.id_category
+
+    # Get products associated with the category id using raw SQL with parameters
+    query = """
+        SELECT p.*
+        FROM api_product AS p
+        JOIN api_productcategory AS pc ON p.id_product = pc.product_category_id
+        WHERE pc.category_product_id = %s
+    """
+    products = Product.objects.raw(query, [category_id])
     return products
+
+def get_products_by_category_and_filters(type, min_price, max_price, size):
+    with connection.cursor() as cursor:
 
 
